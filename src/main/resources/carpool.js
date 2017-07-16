bdb55029d29df963e52d8ffda27f695c6f87f242
Thijs <oostdam@gmail.com>
2017-07-16 15:17:43 +0200

    added surefire plugin for junit 5 tests added some html and javascript to post a trip.

--- src/main/resources/carpool.js
+++ src/main/resources/carpool.js
@@ -1,4 +1,34 @@
+(function(){//IIFE
 
-function someFunction() {
-    document.getElementById('demo').innerHTML = Date();
-}
+    $(function() {
+        $("#submitBtn").click(function(){
+            $.post("/trip",createTrip(), function(){console.log("posted");})
+            .done(function(){console.log("done");})
+            .fail(function() {console.log( "fail" );})
+            .always(function() {console.log( "always" );});
+        });
+    });
+
+    var createTrip = function(){
+        var trip = {
+            maxPassengers : $("#passengers").val(),
+            driver : {
+                email : $("#email").val(),
+                name: $("#name").val()
+            },
+            stops : [{
+                departure : $("#stop1departure").val(),
+                latitude : $("#stop1lat").val(),
+                longitude : $("#stop1long").val()
+            },{
+                departure : $("#stop2departure").val(),
+                latitude : $("#stop2lat").val(),
+                longitude : $("#stop2long").val()
+            }]
+        };
+        return JSON.stringify(trip);
+    }
+
+
+
+})() //IIFE
